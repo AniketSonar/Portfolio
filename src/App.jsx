1518b4a4c28ddad1fc6d56a1ccc53dfabd3cf2a7
AniketSonar <aniketsonar7083@gmail.com>
2026-05-26 16:17:09 +0530

    removed particles effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import Skills from "./components/Skills"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
-import ParticlesBackground from "./components/ParticlesBackground"
-
 import Certifications from "./components/Certifications"
 
 function App() {
   return (
     <div className="relative bg-slate-950 min-h-screen text-white overflow-x-hidden">
 
-      <ParticlesBackground />
+      
 
       <div className="relative z-10">
 
